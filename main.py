02d04bc02cf21c89d3b225f147d5dd9fcd5984b6
Shabd <tadipartyshabd@gmail.com>
2025-12-26 09:57:52 +0530

    Sending fake email to user if anyone enters the frame.

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import cv2
 import time
+from emailing import send_email
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
 
+status_list = []
+
 while True:
+    status = 0
     check, frame = video.read()
+    cv2.imwrite("image.png", frame)
+
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (5, 5), 0)
     cv2.imshow("My Video", gray_frame_gau)
@@ -23,10 +29,22 @@ while True:
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
-        if cv2.contourArea(contour) < 4500:
+        if cv2.contourArea(contour) < 2500:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+        rectangle = cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+        if rectangle.any():
+            status = 1
+
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email()
+
+    print(status_list)
+
 
 
     cv2.imshow('Video', frame)
