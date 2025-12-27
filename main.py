05abea3561f3ddbb2e8f37330f9764d24553a0ea
Shabd <tadipartyshabd@gmail.com>
2025-12-27 12:16:23 +0530

    Found the image with object in it.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
 import time
+import glob
 from emailing import send_email
+
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -9,13 +11,14 @@ first_frame = None
 
 status_list = []
 
+count = 1
+
 while True:
     status = 0
     check, frame = video.read()
-    cv2.imwrite("image.png", frame)
 
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray_frame_gau = cv2.GaussianBlur(gray_frame, (5, 5), 0)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
     cv2.imshow("My Video", gray_frame_gau)
     if first_frame is None:
         first_frame = gray_frame_gau
@@ -24,7 +27,6 @@ while True:
 
     thresh_frame = cv2.threshold(delta_frame, 67, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
-    # cv2.imshow("My Video", dil_frame)
 
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -35,6 +37,11 @@ while True:
         rectangle = cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count += 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images)/2)
+            image_with_object = all_images[index]
 
 
     status_list.append(status)
